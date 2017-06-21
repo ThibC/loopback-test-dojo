b7a646c4a5ea2512f3b18495c4cec227c834edda
Thibault Coudray <th.coudray@gmail.com>
2017-06-21 21:31:53 +0200

    Add test not testing loopback

--- tests/common/models/movie-theater.js
+++ tests/common/models/movie-theater.js
@@ -1,7 +1,6 @@
 'use strict';
 
-var app = require('../../../server/server');
-var utils = require('../../../services/utils');
+var movieTheater = require('../../../common/models/movie-theater');
 var chai = require('chai');
 var expect = chai.expect;
 var sinon = require ('sinon');
@@ -11,23 +10,24 @@ chai.use(sinonChai);
 
 describe('MovieTheater model getAffluenceEvolutionByMonth method', function() {
   var sandbox = null;
+  var MovieTheater = null;
 
-  beforeEach(function() {
-    sandbox = sinon.sandbox.create();
-  });
-
-  afterEach(function() {
-    sandbox = sandbox.restore();
+  before(function() {
+    MovieTheater = {
+      app: {models: {Affluence: {}}},
+      remoteMethod: function() {},
+    };
   });
 
   it('should return the average', function(done) {
-    sandbox.stub(app.models.Affluence, 'find').callsFake(function(filters) {
+    MovieTheater.app.models.Affluence.find = sinon.stub().callsFake(function(filters) {
       return Promise.resolve([{value: 500}, {value: 1000}, {value: 1500}]);
     });
-    app.models.MovieTheater.getAverageAffluenceOverPeriod(1, '2017-06-16', '2017-06-20')
+    movieTheater(MovieTheater);
+    MovieTheater.getAverageAffluenceOverPeriod(1, '2017-06-16', '2017-06-20')
     .then(function(res) {
       expect(res).to.equal(1000);
-      expect(app.models.Affluence.find).to.have.been.calledWith({where: {
+      expect(MovieTheater.app.models.Affluence.find).to.have.been.calledWith({where: {
         movieTheaterId: 1,
         day: {between: ['2017-06-16', '2017-06-20']},
       }});
@@ -36,11 +36,14 @@ describe('MovieTheater model getAffluenceEvolutionByMonth method', function() {
   });
 
   it('should return 0', function(done) {
-    sandbox.stub(app.models.Affluence, 'find').callsFake(function(filters) { return Promise.resolve([]); });
-    app.models.MovieTheater.getAverageAffluenceOverPeriod(0, '2017-06-16', '2017-06-20')
+    MovieTheater.app.models.Affluence.find = sinon.stub().callsFake(function(filters) {
+      return Promise.resolve([]);
+    });
+    movieTheater(MovieTheater);
+    MovieTheater.getAverageAffluenceOverPeriod(0, '2017-06-16', '2017-06-20')
     .then(function(res) {
       expect(res).to.equal(0);
-      expect(app.models.Affluence.find).to.have.been.calledWith({where: {
+      expect(MovieTheater.app.models.Affluence.find).to.have.been.calledWith({where: {
         movieTheaterId: 0,
         day: {between: ['2017-06-16', '2017-06-20']},
       }});
@@ -49,8 +52,10 @@ describe('MovieTheater model getAffluenceEvolutionByMonth method', function() {
   });
 
   it('should return an error', function(done) {
-    sandbox.stub(app.models.Affluence, 'find').callsFake(function(filters) { return Promise.reject('oupsss!'); });
-    app.models.MovieTheater.getAverageAffluenceOverPeriod('yo!', '2017-06-16', '2017-06-20')
+    MovieTheater.app.models.Affluence.find = sinon.stub().callsFake(function(filters) {
+      return Promise.reject('oupsss!');
+    });
+    MovieTheater.getAverageAffluenceOverPeriod('yo!', '2017-06-16', '2017-06-20')
     .then(function() { done('should not been called'); })
     .catch(function(err) {
       expect(err).to.eql('oupsss!');
--- tests/pretest.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-dropdb loopbacktest_test
-createdb loopbacktest_test -O loopbacktest
