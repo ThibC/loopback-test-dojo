cc0be0916796e6f3893dc7481a265e54a7172c29
Thibault Coudray <th.coudray@gmail.com>
2017-06-21 21:01:57 +0200

    Add unit test on promises

--- tests/common/models/movie-theater.js
+++ tests/common/models/movie-theater.js
@@ -2,48 +2,48 @@
 
 var app = require('../../../server/server');
 var utils = require('../../../services/utils');
-var expect = require('chai').expect;
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require ('sinon');
+var sinonChai = require('sinon-chai');
+var Promise = require('bluebird');
+chai.use(sinonChai);
 
 describe('MovieTheater model getAffluenceEvolutionByMonth method', function() {
-  var movieTheaterId = null;
+  var sandbox = null;
 
-  before(function(done) {
-    app.models.MovieTheater.upsert({name: 'UGC Place de Clichy'}, function(err, movieTheater) {
-      if (err) return done(err);
-      movieTheater = utils.detach(movieTheater);
-      movieTheaterId = movieTheater.id;
-      done();
-    });
-  });
-  before(function(done) {
-    app.models.Affluence.upsert({day: '2017-06-17', movieTheaterId: movieTheaterId, value: 1000}, done);
-  });
-  before(function(done) {
-    app.models.Affluence.upsert({day: '2017-06-18', movieTheaterId: movieTheaterId, value: 500}, done);
-  });
-  before(function(done) {
-    app.models.Affluence.upsert({day: '2017-06-19', movieTheaterId: movieTheaterId, value: 500}, done);
+  beforeEach(function() {
+    sandbox = sinon.sandbox.create();
   });
 
-  after(function(done) {
-    app.models.Affluence.destroyAll(function(err) {
-      if (err) return done(err);
-      app.models.MovieTheater.destroyAll(done);
-    });
+  afterEach(function() {
+    sandbox = sandbox.restore();
   });
 
   it('should return the average', function(done) {
-    app.models.MovieTheater.getAverageAffluenceOverPeriod(movieTheaterId, '2017-06-16', '2017-06-20')
+    sandbox.stub(app.models.Affluence, 'find').callsFake(function(filters) {
+      return Promise.resolve([{value: 500}, {value: 1000}, {value: 1500}]);
+    });
+    app.models.MovieTheater.getAverageAffluenceOverPeriod(1, '2017-06-16', '2017-06-20')
     .then(function(res) {
-      expect(res).to.equal(667);
+      expect(res).to.equal(1000);
+      expect(app.models.Affluence.find).to.have.been.calledWith({where: {
+        movieTheaterId: 1,
+        day: {between: ['2017-06-16', '2017-06-20']},
+      }});
       done();
     });
   });
 
   it('should return 0', function(done) {
+    sandbox.stub(app.models.Affluence, 'find').callsFake(function(filters) { return Promise.resolve([]); });
     app.models.MovieTheater.getAverageAffluenceOverPeriod(0, '2017-06-16', '2017-06-20')
     .then(function(res) {
       expect(res).to.equal(0);
+      expect(app.models.Affluence.find).to.have.been.calledWith({where: {
+        movieTheaterId: 0,
+        day: {between: ['2017-06-16', '2017-06-20']},
+      }});
       done();
     });
   });
